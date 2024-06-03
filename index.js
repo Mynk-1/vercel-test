d9c5b3e36184ad88fb5efc040dad0d742a0f98ee
Mayank <legendmayank1628@gmail.com>
2024-06-03 23:36:37 +0530

    tournament backend 1

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const connectToDatabase = require('./db');
-const Item = require('./models/Item.js');
+const bodyParser = require('body-parser');
+const dotenv = require('dotenv');
+const cors = require('cors');
+const User = require("./models/Users")
+
 const app = express();
 const port = process.env.PORT || 3000;
-const dotenv = require('dotenv');
+
 
 // Load environment variables from .env file
 dotenv.config(); 
@@ -11,14 +15,20 @@ dotenv.config();
 // Connect to the database
 connectToDatabase();
 
+app.use(bodyParser.json());
+app.use(cors());
+
 app.get('/', async (req, res) => {
   try {
-    const items = await Item.find({});
+    const items = await User.find({});
     res.send(items);
   } catch (error) {
     res.status(500).send('Error connecting to database');
   }
 });
+
+const authRoutes = require('./routes/auth.js');
+app.use('/api', authRoutes);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
